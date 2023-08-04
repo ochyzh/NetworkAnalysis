71683ce2203d9f62d3c238e889e8ff8f36d62a7f
Olga Chyzh <ochyzh1@gmail.com>
2023-08-04 16:05:41 -0400

    your turns last class

--- public/materials/YourTurnAnswers/14_ego_ergms_yourturn.R
+++ public/materials/YourTurnAnswers/14_ego_ergms_yourturn.R
@@ -1,5 +1,11 @@
+#1. Load the `sampson` dataset from the `sna` library.
 library(sna)
 data(sampson)
+
+#2. How many different roles do you think are in these data?
+
+#3. Estimate an ego ERGM on `samplike` data.
+
 network::list.vertex.attributes(samplike)
 
 m2 <- ego_ergm(net = samplike,
@@ -17,15 +23,28 @@ m2 <- ego_ergm(net = samplike,
                steps = 50,
                tol = 1e-06)
 
+#4. Cross-tabulate the roles you identified with the `group` vertex attribute. Does it seem like the model is picking up differences based on groups?
+
 table(samplike %v% "group",m2$role_assignments[,2])
+
+#5. Cross-tabulate the roles you identified with the `cloisterville` vertex attribute. Does it seem like the model is picking up differences based on whether monks attended Cloisterville?
+
 table(samplike %v% "cloisterville",m2$role_assignments[,2])
+
+#6. Make a network graph, coloring nodes by role and using node size to show group belonging.
 
 
 plot(samplike,
         displaylabels = TRUE,
         # size of nodes based on vector vertexSize
-        vertex.cex = 2*m2$role_assignments[,2],
+        vertex.cex = scales::rescale(degree(samplike), to = c(1,5)),
         # color of nodes based on vertex attribute: group
-        vertex.col = 'group'
+        #vertex.col = 'group',
+     vertex.border = NA,
+     label = m2$role_assignments[,2],
+     label.pos = 5,
+     label.col ="white",
+     vertex.col="group",
+     edge.col="grey40"
 )
 
